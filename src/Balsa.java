8b0bf88f89e4443c14ed14b0291faa795b543f8d
Mario Monzon <mario.monzon.peso@gmail.com>
2025-11-24 08:55:21 +0100

    he añadido la variable nombre a Balsa

--- src/Balsa.java
+++ src/Balsa.java
@@ -1,15 +1,29 @@
 public class Balsa {
 
+    private String nombre;
     private int capacidad;
-    private int tiempo;
+    private double tiempoSegundos;
 
-    public Balsa(int capacidad, int tiempo) {
+    public Balsa(String nombre, int capacidad, double tiempoSegundos) {
+        this.nombre = nombre;
         this.capacidad = capacidad;
-        this.tiempo = tiempo;
+        this.tiempoSegundos = tiempoSegundos;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 
     public int getCapacidad() {
         return capacidad;
     }
 
+    public double getTiempoSegundos() {
+        return tiempoSegundos;
+    }
+
+    @Override
+    public String toString() {
+        return "Balsa{ nombre='" + nombre + '\'' +", capacidad=" + capacidad +", tiempoSegundos=" + tiempoSegundos ;
+    }
 }
