0d4cc1dd738d0694daaacc4f87e899591f37911b
Mario Monzon <mario.monzon.peso@gmail.com>
2025-11-24 09:08:30 +0100

    He sustituido el metodo hay pasajeros() por pasajerosRestantes() para que te devuelva elnumero exacto y no boolean. Creo un metodo recatar, que contiene las variables county prioridad.Con el while lo recorre todos los pasajeros por prioridad. Con el for recorre la lista de pasajeros y va rescatando por menor prioridad,una vez se acaba pasa a la siguiente prioridad

--- src/Barco.java
+++ src/Barco.java
@@ -3,14 +3,41 @@ import java.util.List;
 
 public class Barco {
 
-    private final List<Pasajero> pasajeros = new ArrayList<>();
+    private List<Pasajero> pasajeros;
 
     public Barco(List<Pasajero> pasajeros) {
-        this.pasajeros.addAll(pasajeros);
+        this.pasajeros = new ArrayList<>(pasajeros);
     }
 
-    public boolean hayPasajeros(){
-        return !pasajeros.isEmpty();
+    public List<Pasajero> rescatar(int capacidad) {
+        List<Pasajero> rescatados = new ArrayList<>();
+        int count = 0;
+        int prioridad = 1;
+
+        // Mientras queden prioridades por revisar y no se llene la capacidad
+        while (count < capacidad && prioridad <= 4) {
+
+            for (int i = 0; i < pasajeros.size() && count < capacidad; i++) {
+                Pasajero p = pasajeros.get(i);
+
+                if (p.getPrioridad() == prioridad) {
+                    rescatados.add(p);
+                    pasajeros.remove(i);
+                    i--;
+                    count++;
+                }
+            }
+
+            prioridad++;
+        }
+
+        return rescatados;
     }
 
+    public int pasajerosRestantes() {
+        return pasajeros.size();
+    }
 }
+
+
+
