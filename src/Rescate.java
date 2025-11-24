8ac2d77600ed3f133bc488974b48bfa35d33202b
Mario Monzon <mario.monzon.peso@gmail.com>
2025-11-24 09:16:37 +0100

    Creo las variables Balsa y Barco y genero un constructor. Dentro del while del run meto un trycatch. Creo un array con con todos los pasajeros y compruebo q no esta vacio. Después hago que cada balsa empieze a rescatar indicando el nombre de la misma y ha quien rescata. Unas vez termina se le hace esperar  su tiempo correspondiente a cada balsa para que pueda volver a rescatar

--- src/Rescate.java
+++ src/Rescate.java
@@ -1,8 +1,51 @@
-public class Rescate implements Runnable{
+import java.util.List;
 
-    public Rescate(){}
+public class Rescate implements Runnable {
+
+    private Balsa balsa;
+    private Barco barco;
+
+    public Rescate(Balsa balsa, Barco barco) {
+        this.balsa = balsa;
+        this.barco = barco;
+    }
+
     @Override
     public void run() {
-        while(true){}
+        while (true) {
+            try {
+                // capacidad de la balsa
+                List<Pasajero> rescatados = barco.rescatar(balsa.getCapacidad());
+
+                if (rescatados.isEmpty()) {
+                    // No quedan pasajeros
+                    break;
+                }
+
+
+                // rescate de cada balsa
+
+                if (!rescatados.isEmpty()) {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("Balsa ").append(balsa.getNombre())
+                            .append(" ha rescatado ").append(rescatados.size()).append(" pasajeros: ");
+
+                    for (Pasajero p : rescatados) {
+                        sb.append(p.getId()).append(" ");
+                    }
+
+                    // Imprime toda la línea de golpe
+                    System.out.println(sb.toString());
+                }
+
+
+                // tiempo de rescate
+                Thread.sleep((long) (balsa.getTiempoSegundos() * 1000));
+
+                System.out.println("Balsa regresada: " + balsa.getNombre());
+            } catch (InterruptedException ex) {
+                System.getLogger(Rescate.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        }
     }
 }
